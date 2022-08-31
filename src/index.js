19073745b88b7990556d9efa3108a61f581495bc
Debby Miressa <101793279+DebbyMiressa@users.noreply.github.com>
2022-08-31 23:42:34 +0300

    Implement Refresh Leaderboard
    - Add refresh.js module to GET data in JSON format from the URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/sj3CaiiOl4GBjioetz1v/scores'
    - Add result as new <li> in <ul>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import Refresh from './refresh.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const Ref = Refresh;
+  const refObj = new Ref();
+  refObj.refresh();
+});
